ee66a0339bda5628ccd1499112749f8973e88e2e
lollobianco <loreb2002@gmail.com>
2022-11-07 15:13:09 +0100

    Primo array snack 2 completato

---
+++ snack2/main.js
@@ -0,0 +1,53 @@
+// Snack 2
+
+// Dato un elenco degli studenti di una facoltà, con il totale dei loro voti
+// let students = [
+//     { name: 'Marco', id: 213, grades: 78 },
+//     { name: 'Paola', id: 110, grades: 96 },
+//     { name: 'Andrea', id: 250, grades: 48 },
+//     { name: 'Gaia', id: 145, grades: 74 },
+//     { name: 'Luigi', id: 196, grades: 68 },
+//     { name: 'Piero', id: 102, grades: 50 },
+//     { name: 'Francesca', id: 120, grades: 84 },
+//   ];
+// 1- Dobbiamo creare delle targhe con il loro nome in maiuscolo. Ci serve quindi un nuovo array di stringhe. Ogni elemento del nuovo array sarà il nome dello studente ma con tutte le lettere maiuscole.
+// 2-  Dobbiamo creare un nuovo array con gli studenti che hanno un totale di voti superiore a 70
+// 3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+
+let students = [
+   { name: 'Marco', id: 213, grades: 78 },
+   { name: 'Paola', id: 110, grades: 96 },
+   { name: 'Andrea', id: 250, grades: 48 },
+   { name: 'Gaia', id: 145, grades: 74 },
+   { name: 'Luigi', id: 196, grades: 68 },
+   { name: 'Piero', id: 102, grades: 50 },
+   { name: 'Francesca', id: 120, grades: 84 },
+];
+
+let upperNames = [];
+let gradesAndId = [];
+//console.log(students)
+
+//ARRAY NOMI IN MAIUSCOLO
+
+students.forEach((element) => {
+
+   upperNames.push(element.name.toUpperCase())
+
+})
+
+console.log(`Nomi Studenti (${upperNames.length}): ${upperNames}`)
+
+
+
+//ARRAY STUDENTI CON UN TOTALE DI VOTI SUPERIORE A 70
+
+let niceGrades = students.filter((element) => {
+
+   return element.grades > 70;
+
+});
+
+console.log(niceGrades.name);
+
+console.log (`Studenti con il voto superiore a 70 (${niceGrades.length}): ${niceGrades}`)
