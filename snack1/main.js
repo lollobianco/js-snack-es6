106f244ab48853374ef8bbc52b2017c66424d068
lollobianco <loreb2002@gmail.com>
2022-11-07 14:35:00 +0100

    Snack-1 completato

--- snack1/main.js
+++ snack1/main.js
@@ -9,7 +9,7 @@
 // esempio: dati i valori min: 2 e come max: 4 i nomi nell'array nuovo saranno i nomi compresi tra queste posizioni 2 e 4
 
 const myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
-const yourArray = [];
+const yourArrayForEach = [];
 
 let min = prompt('Inserisci il minimo: ');
 let max = prompt('Inserisci il massimo: ');
@@ -17,9 +17,9 @@ let max = prompt('Inserisci il massimo: ');
 
 //CONTROLLO DEL MINIMO E DEL MASSIMO
 
-while (min < 0 || max > myArray.length - 1){
+while (min < 0 || max > myArray.length - 1 || min == max){
 
-   alert('il valori devono essere compresi tra 0 e 5')
+   alert('il valori devono essere compresi tra 0 e 5 e diversi tra loro')
 
    min = prompt('Inserisci il minimo: ');
    max = prompt('Inserisci il massimo: ');
@@ -32,12 +32,28 @@ while (min < 0 || max > myArray.length - 1){
 myArray.forEach((element, index) => {
 
    if (index >= min && index <= max){
-      yourArray.push(element);
+      yourArrayForEach.push(element);
    }
 
-   return yourArray;
+   return yourArrayForEach;
 });
 
-console.log(yourArray)
+console.log(`Array con forEach: ${yourArrayForEach}`)
 
 
+
+//CREAZIONE DELL'ARRAY IN BASE AI NUMERI INSERITI DALL'UTENTE (VERSIONE CON FILTER)
+
+const yourArrayFilter = myArray.filter((element, index) => {
+
+   if (index >= min && index <= max){
+
+      return true;
+
+   }
+
+   return false;
+
+});
+
+console.log(`Array con Filter: ${yourArrayFilter}`)
