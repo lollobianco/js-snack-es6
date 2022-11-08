476f8d4954f0785573f6ad4c4658dd0e29869fdc
lollobianco <loreb2002@gmail.com>
2022-11-08 15:25:14 +0100

    Creato array di bici snack3 e tentativi di estrarre il peso

--- snack3/main.js
+++ snack3/main.js
@@ -1,2 +1,25 @@
 // Snack 3
 // Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+const bikes = [
+   {
+      nome: 'biciUno',
+      peso: 5
+   },
+   {
+      nome: 'biciDue',
+      peso: 10
+   },
+   {
+      nome: 'biciTre',
+      peso: 7
+   },
+   {
+      nome: 'biciQuattro',
+      peso: 12
+   }
+];
+
+let { nome, peso } = bikes;
+
+console.log(peso);
